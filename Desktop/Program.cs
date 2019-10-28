d8e111c60c072a8f6a4494cecf6d345b5ea7c45b
Jason Pang <jppang@ucdavis.edu>
2019-10-27 20:24:18 -1000

    Update binary build files for Linux ARM

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Commons.Music.Midi;
+using System;
+using System.Linq;
 
 namespace Desktop
 {
@@ -6,11 +8,33 @@ namespace Desktop
     {
         static void Main()
         {
-            var engine = new DesktopEngine();
-            engine.Server.Run(engine);
+            try
+            {
+                var midiAccess = new Commons.Music.Midi.RtMidi.RtMidiAccess();
+                Console.WriteLine("Using RtMidi.");
+                foreach (var input in midiAccess.Inputs)
+                {
+                    Console.WriteLine($"Input: {input.Id} - {input.Name}");
+                }
+                foreach (var output in midiAccess.Outputs)
+                {
+                    Console.WriteLine($"Output: {output.Id} - {output.Name}");
+                }
 
-            Console.Write("Press <ENTER> to exit the program...");
-            Console.ReadLine();
+                IMidiPortDetails matchedInput = null;
+                do
+                {
+                    Console.Write("Select Input ID:");
+                    var inputId = Console.ReadLine();
+                    matchedInput = midiAccess.Inputs.ToList().Find(x => x.Id.ToLower().Contains(inputId));
+                } while (matchedInput != null);
+
+                midiAccess.OpenInputAsync(matchedInput.Id).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught exception: {ex.Message} \n\n {ex.InnerException} \n\n {ex.StackTrace}");
+            }
         }
     }
 }
