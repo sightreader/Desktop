2de2490018380f47ed364a5aa8bea79d57e7ca10
Jason Pang <jppang@ucdavis.edu>
2019-10-27 14:21:12 -1000

    Initial commit
    Publish alpha Linux framework dependent build for .NET Core 3.0.

---
+++ Desktop/DesktopEngine.cs
@@ -0,0 +1,20 @@
+﻿using Commons.Music.Midi;
+using SightReader.Engine;
+using SightReader.Engine.Interpreter;
+using SightReader.Engine.Introducer;
+using SightReader.Engine.Server;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desktop
+{
+    public class DesktopEngine : IEngineContext
+    {
+        public List<IMidiInput> MidiInputs { get; set; } = new List<IMidiInput>();
+        public List<IMidiOutput> MidiOutputs { get; set; } = new List<IMidiOutput>();
+        public Interpreter Interpreter { get; set; } = new Interpreter();
+        public IntroducerClient Introducer { get; set; } /* Not implemented */
+        public CommandServer Server { get; set; } = new CommandServer();
+    }
+}
---
+++ Desktop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Commons.Music.Midi;
+using System.Linq;
+using SightReader.Engine.ScoreBuilder;
+using SightReader.Engine.Interpreter;
+using System.Collections.Generic;
+using System.IO;
+using SightReader.Engine.Server;
+
+namespace Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new DesktopEngine();
+            engine.Server.Run(engine);
+
+            Console.Write("Press <ENTER> to exit the program...");
+            Console.ReadLine();
+        }
+    }
+}
