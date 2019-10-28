29fbdd619dc843ea4128f3a3459d123bf845caff
Jason Pang <jppang@ucdavis.edu>
2019-10-27 20:36:32 -1000

    Display input messages from ALSA

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -29,12 +29,22 @@ namespace Desktop
                     matchedInput = midiAccess.Inputs.ToList().Find(x => x.Id.ToLower().Contains(inputId));
                 } while (matchedInput == null);
 
-                midiAccess.OpenInputAsync(matchedInput.Id).Wait();
+                var openedInput = midiAccess.OpenInputAsync(matchedInput.Id).Result;
+
+                openedInput.MessageReceived += Input_MessageReceived;
+
+                Console.WriteLine("MIDI Input Opened. Press <ENTER> to close program.");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Caught exception: {ex.Message} \n\n {ex.InnerException} \n\n {ex.StackTrace}");
             }
+        }
+
+        private static void Input_MessageReceived(object sender, MidiReceivedEventArgs e)
+        {
+            Console.WriteLine($"Input Message: {e.Data}");
         }
     }
 }
